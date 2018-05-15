c3e0346f07ec9a10d162b3d4b1a4e5a2b3541010
liwencheng <liwencheng@hisense.com>
2018-05-15 16:50:41 +0800

    Description:add a 4 bit full-adder and test Type:New

--- add/add.v
+++ add/add.v
@@ -8,3 +8,38 @@ module add_full_1(co,v,a,b,ci);
     and a2(c2, ci, add1);
     or  o1(co, c1, c2);
 endmodule
+
+
+
+module add_full_4(co,s,a,b,ci);
+    output co;
+    reg co;
+    output [3:0] s;
+    reg [3:0] s;
+    input ci;
+    input [3:0] a, b;
+    reg   [3:0] G, P;
+    reg   [2:0] C;
+    always @(a, b, ci)
+    begin
+    P[0] = a[0] ^ b[0];
+    P[1] = a[1] ^ b[1];
+    P[2] = a[2] ^ b[2];
+    P[3] = a[3] ^ b[3];
+    
+    G[0] = a[0] & b[0];
+    G[1] = a[1] & b[1];
+    G[2] = a[2] & b[2];
+    G[3] = a[3] & b[3];
+
+    C[0] = G[0] | (P[0]&ci);
+    C[1] = G[1] | (P[1]&G[0]) | (P[1]&P[0]&ci);
+    C[2] = G[2] | (P[2]&G[1]) | (P[2]&P[1]&G[0]) | (P[2]&P[1]&P[0]&ci);
+    co   = G[3] | (P[3]&G[2]) | (P[3]&P[2]&G[1]) | (P[3]&P[2]&P[1]&G[0]) | (P[3]&P[2]&P[1]&P[0]&ci);
+
+    s[0] = P[0] ^ ci;
+    s[1] = P[1] ^ C[0];
+    s[2] = P[2] ^ C[1];
+    s[3] = P[3] ^ C[2];
+    end
+endmodule
--- add/testbench.v
+++ add/testbench.v
@@ -3,12 +3,19 @@ module testbench();
     reg a, b;
     wire co1, v1;
     wire co2, v2;
+    wire c1, c2;
+    wire [3:0] s1, s2;
+    reg  [3:0] a1, a2, b1, b2;
     initial
     begin
         $dumpfile("add.vcd");
         $dumpvars;
         a = 1'b0;
         b = 1'b0;
+        a1 = 4'b0101;
+        a2 = 4'b1111;
+        b1 = 4'b1010;
+        b2 = 4'b0001;
         #100
         $finish;
     end
@@ -19,6 +26,8 @@ module testbench();
         #10
         b = ~b;
     end
-    add_full_1 a1(co1, v1, a, b, 1'b0);
-    add_full_1 a2(co2, v2, a, b, 1'b1);
+    add_full_1 a_1_1(co1, v1, a, b, 1'b0);
+    add_full_1 a_1_2(co2, v2, a, b, 1'b1);
+    add_full_4 a_4_1(c1, s1, a1, b1, 1'b0);
+    add_full_4 a_4_2(c2, s2, a2, b2, 1'b1);
 endmodule
